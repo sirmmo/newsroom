a5aa3ee7cf812b84acb4442133c3bcd085a07ce3
Marco Montanari <sirmmo@gmail.com>
2025-10-08 17:50:12 +0200

    update

--- newsroom/settings.py
+++ newsroom/settings.py
@@ -137,3 +137,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
